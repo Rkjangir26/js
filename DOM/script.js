7d126e16cf289a71c91b0959d568d7f554be1d27
ravikumar26-jangir <ravijangir741@hotmail.com>
2024-02-17 10:59:13 +0530

    pratice q in js

---
+++ DOM/script.js
@@ -0,0 +1,27 @@
+// let heading = document.getElementById("heading");
+// console.dir(heading)
+// console.log(heading)
+
+
+//  heading=document.getElementsByClassName("heading");
+//  console.dir(heading)
+// console.log(heading)
+
+
+// let para =document.getElementsByTagName("p");
+// console.log(para);
+// console.dir(para)
+
+
+// let elements = document.querySelectorAll("h1");
+// console.log(elements);
+// console.dir(elements)
+
+
+
+// let h2 = document.querySelector("h2");
+
+
+
+// h2.innerText = h2.innerText + "from rkjangir";
+// console.dir(h2.innerText);
